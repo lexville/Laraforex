025ffe51a9eb28db7825b42320a8f73bb601d354
lexville <alex.kangethe@andela.com>
2016-08-04 13:13:34 +0300

    (chore) Add search bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('welcome')
 
 @section('content')
-    <h3 class="text-center">Search For Currency</h3>
+    <h3 class="text-center">Convert currency</h3>
+    {!! Form::open(array()) !!}
+
+    <div class="form-group">
+        {!! Form::label('currency', 'Amount:', ['class' => 'control-label']) !!}
+        {!! Form::text('currency', null, ['class' => 'form-control', 'placeholder' => 'Enter amount to be converted']) !!}
+    </div>
+    {!! Form::submit('Search', ['class' => 'btn btn-raised btn-primary']) !!}
+    {!! Form::close() !!}
 @stop
