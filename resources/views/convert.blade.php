e3deedf554e8b543f61d80bdbcfbee9159bd813f
lexville <alex.kangethe@andela.com>
2016-08-04 14:32:36 +0300

    (Chore) Add code names

--- resources/views/convert.blade.php
+++ resources/views/convert.blade.php
@@ -8,28 +8,34 @@
     <table class="table table-hover">
     <thead>
       <tr>
+        <th>Name</th>
         <th>Code</th>
         <th>Currency</th>
       </tr>
     </thead>
     <tbody>
       <tr>
+        <td>Euro</td>
         <td>EUR</td>
         <td>@currency($query, 'EUR')</td>
       </tr>
       <tr>
+        <td>Pound Sterling</td>
         <td>GBP</td>
         <td>@currency($query, 'GBP')</td>
       </tr>
       <tr>
+        <td>Hong Kong Dollar</td>
         <td>HKD</td>
         <td>@currency($query, 'HKD')</td>
       </tr>
       <tr>
+        <td>Japanese Yen</td>
         <td>JPY</td>
         <td>@currency($query, 'JPY')</td>
       </tr>
       <tr>
+        <td>South African rand</td>
         <td>ZAR</td>
         <td>@currency($query, 'ZAR')</td>
       </tr>
