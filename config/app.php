38aec39d9276726663b21fa882bd0aa832a3d930
lexville <alex.kangethe@andela.com>
2016-08-04 13:01:07 +0300

    (Feature) Add asinelli/Currency package

--- config/app.php
+++ config/app.php
@@ -157,6 +157,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Casinelli\Currency\CurrencyServiceProvider::class,
 
     ],
 
@@ -203,6 +204,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Currency' => Casinelli\Currency\Facades\Currency::class,
+        'Input' => Illuminate\Support\Facades\Input::class,
+
 
     ],
 
