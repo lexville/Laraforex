a5bf1c9c012148a88ae6b5a93862a0d40f23975c
lexville <alex.kangethe@andela.com>
2016-08-04 13:44:51 +0300

    (Chore) Add currency drop down

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,10 @@
         {!! Form::label('currency', 'Amount:', ['class' => 'control-label']) !!}
         {!! Form::text('currency', null, ['class' => 'form-control', 'placeholder' => 'Enter amount to be converted']) !!}
     </div>
+    <div class="form-group">
+        {!! Form::label('currency-code', 'Currency Code:', ['class' => 'control-label']) !!}
+        {!! Form::select('currency-code', array('EUR', 'GBP', 'HKD', 'JPY', 'ZAR', 'CAD'), NULL, array('class' => 'form-control')); !!}
+    </div>
     {!! Form::submit('Search', ['class' => 'btn btn-raised btn-primary']) !!}
     {!! Form::close() !!}
 @stop
