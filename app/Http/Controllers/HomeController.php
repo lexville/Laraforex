2327faa977d75b804b7ab22995ec197119653467
lexville <alex.kangethe@andela.com>
2016-08-04 11:48:26 +0300

    (Fix) Change the controller name

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-class HomeConttroller extends Controller
+class HomeController extends Controller
 {
     /**
      * Display a listing of the resource.
